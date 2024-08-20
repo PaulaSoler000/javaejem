70867355e9213a33dc8b5aa246f9bdfd816b7526
PaulaSoler000 <paulasoler111@gmail.com>
2024-08-20 19:35:16 +0200

    hola

---
+++ app/hello.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Esta clase se llama HelloWorld en vez de Main para mantener la coherencia con el proyecto
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+}
